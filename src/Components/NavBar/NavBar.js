3113eb4b4aa934f2e58be1fb7a1dc72349f09cbc
MaksMalik <maksymilian.malik@gmail.com>
2022-09-29 20:22:25 +0200

    Edit navbar mobile

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,16 +7,16 @@ export default function NavBar() {
       <div className='navbar'>
         <div className='navbar_title'>Connect</div>
         <div className='navbar_navigation'>
-          <div className='navbar_navigation_element'>How?</div>
-          <div className='navbar_navigation_element'>Prices</div>
-          <div className='navbar_navigation_element'>Download</div>
-          <div className='navbar_navigation_element'>Contact</div>
+          <a href='#how'><div className='navbar_navigation_element'>How?</div></a>
+          <a href='#prices'><div className='navbar_navigation_element'>Prices</div></a>
+          <a href='#download'><div className='navbar_navigation_element'>Download</div></a>
+          <a href='#how'><div className='navbar_navigation_element'>Contact</div></a>
         </div>
         <div className='navbar_navigation_mobile'>
-          <div className='navbar_navigation_element_mobile questionMark'/>
-          <div className='navbar_navigation_element_mobile prices'/>
-          <div className='navbar_navigation_element_mobile download'/>
-          <div className='navbar_navigation_element_mobile contact'/>
+          <a href='#how'><div className='navbar_navigation_element_mobile questionMark'/></a>
+          <a href='#prices'><div className='navbar_navigation_element_mobile prices' /></a>
+          <a href='#download'><div className='navbar_navigation_element_mobile download' /></a>
+          <a href='#how'><div className='navbar_navigation_element_mobile contact' /></a>
         </div>
       </div>
     </>
