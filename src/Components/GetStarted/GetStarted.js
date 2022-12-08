0e38c81f3b2e847bf3d09bd29ed0e18df9589227
Maksymilian Malik <maksymilian.malik@gmail.com>
2022-12-09 00:13:07 +0100

    ad

--- src/Components/GetStarted/GetStarted.js
+++ src/Components/GetStarted/GetStarted.js
@@ -9,7 +9,7 @@ export default function GetStarted() {
         <div className='getStarted_image1' />
         <div className='getStarted_title'>Secure yourself</div>
         <div className='getStarted_subTitle1'>VPN that you can trust</div>
-        <Button href="#prices" className='getStarted_button' variant='outlined'>Get Started</Button>
+        <Button href="#how" className='getStarted_button' variant='outlined'>Get Started</Button>
         <div className='getStarted_subTitle2'>30 days free premium trial</div>
       </div>
     
