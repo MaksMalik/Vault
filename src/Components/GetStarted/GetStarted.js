fb721579e9998670649d3ce024461f5e3cab3b75
Maksymilian Malik <maksymilian.malik@gmail.com>
2023-01-28 01:12:12 +0100

    ee

--- src/Components/GetStarted/GetStarted.js
+++ src/Components/GetStarted/GetStarted.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import "./GetStarted.scss"
 
 export default function GetStarted() {
@@ -10,34 +10,47 @@ export default function GetStarted() {
   });
 
 
-  const [currentPage, setCurrentPage] = useState("start")
-  const sections = document.querySelectorAll(".section")
-  const links = document.querySelectorAll(".link")
-  useEffect(() => {
-    window.onscroll = function () {
-      sections.forEach((section) => {
-        let sectionTop = section.offsetTop;
-        if (window.pageYOffset >= sectionTop - 200) {
-          setCurrentPage(section.getAttribute("id"))
-        }
-      })
 
-      links.forEach((item) => {
-        item.style.cssText = "color: white"
-        if (item.href.includes(currentPage)) {
-          item.style.cssText = "color: white";
-          item.classList.remove("active")
-        }
-        else {
-          item.style = "color: black"
-        }
-      })
-    }
+  
+  
+  
+  window.addEventListener('scroll', function () {
+    
+    let cloud = document.querySelectorAll(".cloud")
+    let getStarted = document.querySelectorAll(".getStarted")
+
+    let cloud2 = document.querySelectorAll(".cloud2")
+
+    let value = window.scrollY
+    cloud.forEach((item) => {
+      item.style.translate = `${-value * 0.5}px 0`
+      item.style.cssText = `translate: ${-value}px 0 ; scale: ${1 + value / 1000}`
+    })
+
+    cloud2.forEach((item) => {
+      item.style.translate = `${value * 0.5}px 0`
+      item.style.cssText = `translate: ${value}px 0; scale: ${1 + value / 1000}`
+    })
+
+    getStarted.forEach((item) => {
+      item.style.translate = `0 ${value * 0.5}px`
+    })
   })
+
+
+
+
+
+
+
+
   return (
     <>
       <div className='getStarted section' id='start'>
         <div className='getStarted-wrap'>
+          <div className='cloud'></div>
+          <div className='cloud2'></div>
+
           <div className='getStarted_title'>Secure yourself</div>
           <div className='getStarted_subTitle1'>VPN that you can trust</div>
           <a href='#how'>
