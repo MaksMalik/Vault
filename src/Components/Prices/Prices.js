a728a152af2d38e19bcedd6f96ffd4078e3064e1
Maksymilian Malik <maksymilian.malik@gmail.com>
2023-01-28 01:20:44 +0100

    ee

--- src/Components/Prices/Prices.js
+++ src/Components/Prices/Prices.js
@@ -4,11 +4,13 @@ import "./Prices.css"
 export default function Prices() {
   return (
     <>
-      <div className='prices section' id='prices'>
+        <div className='prices section' id="prices">
+          <div className='HowItWorks_title-top'>Proin nec ante eros. </div>
+
+          <div className='HowItWorks_title'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Nam accumsan sem ut accumsan commodo. <br /> Et harum quidem rerum facilis est et expedita distinctio. </div>
+        </div>
 
 
-
-      </div>
     </>
   )
 }
